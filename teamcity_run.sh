0cbefd27de0237757b9a7032b801253ab8b5fdbd
Stefano Pirrello <spirrello@liaison.com>
2019-04-09 21:49:38 -0400

    fixed shell scripts

--- teamcity_run.sh
+++ teamcity_run.sh
@@ -5,7 +5,7 @@ IMAGE="jetbrains/teamcity-server:${VERSION}"
 docker pull $IMAGE
 
 exec docker run -it --name teamcity-server-instance  \
---name "teamcity-server"
+--name "teamcity-server" \
 --detach \
 -v datadir:/data/teamcity_server/datadir \
 -v logs:/opt/teamcity/logs  \
